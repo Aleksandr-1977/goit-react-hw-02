a19a374a36e2d1d990e95820e2ee1af9b4064179
Aleksandr-1977 <vasilenkobox@gmail.com>
2025-03-01 20:05:23 +0200

    full HW

--- src/components/HW-1/Task/Task1.jsx
+++
@@ -1,5 +0,0 @@
-import css from './Task.module.css';
-const Task1 = () => {
-  return <h2 className={css.text}>Task 1 </h2>;
-};
-export default Task1;
--- src/components/HW-1/Task/Task3.jsx
+++
@@ -1,5 +0,0 @@
-import css from './Task.module.css';
-const Task3 = () => {
-  return <h2 className={css.text}>Task 3 </h2>;
-};
-export default Task3;
